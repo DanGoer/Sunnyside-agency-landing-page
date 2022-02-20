5bb85c06378fbbe25e9d5faca9dbd8b2a3f13dec
DanGoer <87978392+DanGoer@users.noreply.github.com>
2022-02-20 12:58:27 +0100

    Final solution

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const burgerMenu = document.getElementById("burger-menu");
 const menu = document.getElementById("menu");
 burgerMenu.addEventListener("click", function () {
   this.classList.toggle("close");
-  overlay.classList.toggle("overlay");
+  menu.classList.toggle("overlay");
 });
